f7b97df834705577eb9198b873a274b9eb2daae5
Nour Khaled <100867406+Nourk4441@users.noreply.github.com>
2024-12-08 14:19:05 +0200

    Update script3.js

--- Lab1-js/script3.js
+++ Lab1-js/script3.js
@@ -1,11 +1,21 @@
-cond=false
-while(cond=false){   
-name1=prompt('Enter your name')
-if(typeof name1==="string"){cond=true}
-birth=prompt('Enter birth year')
-if(typeof birth==="number"& birth<2010){cond=true}
-age=2010-birth
-    document.write('Name: '+name1)
-    document.write('Birth year: '+birth)
-    document.write('Age: '+age)
+let cond = false;
+
+while (!cond) {
+    let name1 = prompt('Enter your name');
+    if (typeof name1 === "string" && name1.trim() !== "" && /^[a-zA-Z]+$/.test(name1)) {
+        let birth = parseInt(prompt('Enter birth year'), 10);
+        if (!isNaN(birth) && birth < 2010) {
+            cond = true;
+            let age = 2010 - birth;
+
+            // Writing the output to the document
+            document.write('<p>Name: ' + name1 + '</p>');
+            document.write('<p>Birth year: ' + birth + '</p>');
+            document.write('<p>Age: ' + age + '</p>');
+        } else {
+            alert('Invalid birth year. Please enter a valid number less than 2010.');
+        }
+    } else {
+        alert('Invalid name. Please enter a valid string containing only letters.');
+    }
 }
